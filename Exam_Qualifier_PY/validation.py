e119288323629fedbfec30dd2ac6cd5637f7d921
bnkala <nkala.bryan3@gmail.com>
2022-08-03 15:36:41 +0200

    working on python project of qualification

--- Exam_Qualifier_PY/validation.py
+++
@@ -1,9 +0,0 @@
-def is_input_valid(question):
-    if question not in ["y", "Y".lower(), "yes", "no", "n", "No".lower()]:
-        return False
-    return True 
-
-def is_num_valid(num):
-    if not num.isdigit():
-        return False
-    return True
