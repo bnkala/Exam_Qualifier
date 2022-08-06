031b0a785b05e684e6842f6fa88ee7200dfa1dbc
bnkala <nkala.bryan3@gmail.com>
2022-08-06 18:28:35 +0200

    #completed scores

--- Exam_Qualifier_PY/main.py
+++ Exam_Qualifier_PY/main.py
@@ -1,5 +1,6 @@
 
 from itertools import count
+import test
 
 '''
 def get_name():
@@ -14,6 +15,9 @@ def get_institution():
 def get_qualification():
     return input("Enter qualification you studying for: ")'''
 
+"""
+Test score(s)
+"""
 def get_test_validation():
     return input("Have you written tests this semester(y/n)? ")
 
@@ -21,7 +25,7 @@ def count_test():
     return int(input("How many tests did you write? "))
 
 def test_score(count):
-    return int(input("Enter score for test {}: ".format(count+1)))
+    return int(input("Enter score (%) for test {}: ".format(count+1)))
 
 def get_total_test(test_validation):
     test_count = 0
@@ -39,19 +43,64 @@ def get_total_test(test_validation):
     
     return tot_test
 
-'''
+"""
+Project score(s)
+"""
 def get_project_validation():
     return input("Have you worked on projects this semester(y/n)? ")
 
 def count_project():
     return int(input("How many projects have you worked on? "))
 
+def project_score(count):
+    return int(input("Enter score (%) for project {}: ".format(count+1)))
+
+def get_total_project(validation):
+    project_count = 0
+    tot_project = 0
+    if validation in ["y","Y"]:
+        project_count = count_project()
+        if project_count <= 0:
+            return tot_project
+        else:
+            for i in range(project_count):
+                tot_project += project_score(i)
+            tot_project = tot_project/project_count
+    if validation in ["n", "N"]:
+        return tot_project
+    
+    return tot_project
+
+"""
+Presentation Score(s)
+"""
+
 def get_presentation_validation():
     return input("Have you done any presentations this semester(y/n)? ")
 
 def count_presentation():
     return int(input("How many presentations have you done? "))
-'''
+
+def presentation_score(count):
+    return int(input("Enter score (%) for presentation {}:".format(count+1)))
+
+def get_total_presentation(validation):
+    presentation_count = 0
+    tot_presentation = 0
+    if validation in ["y","Y"]:
+        presentation_count = count_presentation()
+        if presentation_count <=0:
+            return tot_presentation
+        else:
+            for i in range(presentation_count):
+                tot_presentation += presentation_score(i)
+            tot_presentation = tot_presentation/presentation_count
+    if validation in ["n","N"]:
+        return tot_presentation
+    
+    return tot_presentation
+
+
 
 def get_exam():
     return int(input("Enter exam mark: "))
@@ -60,8 +109,40 @@ def get_final_mark(dp_mark, exam):
     return (dp_mark*0.4) + (exam*0.6)
 
 def display():
-    validation = get_test_validation()
-    print("Total test score out of 100: {}".format(get_total_test(validation)))
+    tot_dp = 0
+    tot_count = 0
+    avg_dp = 0
+    #print("Total test score out of 100: {}".format(get_total_test(validation)))
+
+    test_validation = get_test_validation()
+    if test_validation in ["y","Y"]:
+        tot_dp += get_total_test(test_validation)
+        tot_count += 1
+
+    project_validation = get_project_validation()
+    if project_validation in ["y","Y"]:
+        tot_dp += get_total_project(project_validation)
+        tot_count += 1
+
+    presentation_validation = get_presentation_validation()
+    if presentation_validation in ["y","Y"]:
+        tot_dp += get_total_presentation(presentation_validation)
+        tot_count += 1
+    
+    if(tot_count == 0):
+        print("Nothing has been recorded")
+        exit()
+
+    avg_dp = tot_dp / tot_count    
+
+    exam = get_exam()
+
+    final_mark = get_final_mark(avg_dp, exam)
+
+    print("{}%".format(final_mark))
+
+    
+
     
 if __name__=="__main__":
     display()
